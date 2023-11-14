965b5e914e58cb09574f74cdc971f893f3f75e92
Abomi Daba <abomi.daba@gmail.com>
2023-11-14 09:59:15 -0800

    initial commit

--- plusesAndMinuses.js
+++ plusesAndMinuses.js
@@ -1,2 +1,16 @@
+let count = document.getElementById('count');
+let counter = 0;
+const plusOrMinus = (e) => {
+    if (e.target.id === 'plus') {
+        counter++;
+    } else if (e.target.id === 'minus') {
+        counter--;
+    }
+    count.innerText = counter;
+}
 // When a user clicks the + element, the count should increase by 1 on screen.
+let plusBotton = document.querySelector('button');
+plusBotton.addEventListener('click', (e) => plusOrMinus(e))
 // When a user clicks the – element, the count should decrease by 1 on screen.
+let minusButton = document.getElementById('minus');
+minusButton.addEventListener('click', (e) => plusOrMinus(e));
